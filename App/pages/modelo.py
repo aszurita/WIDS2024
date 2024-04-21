8dd8b2bf5aba400e9c3ad5659497bcd7f9565f3d
aszurita <nzuritag@fiec.espol.edu.ec>
2024-04-20 21:06:28 -0500

    Estructura  Inicial Proyecto

---
+++ App/pages/modelo.py
@@ -0,0 +1,8 @@
+from dash import Dash, dcc, html
+from dash.dependencies import Input, Output, State
+from dash.exceptions import PreventUpdate
+import dash_bootstrap_components as dbc
+import plotly.express as px
+import pandas as pd
+
+layout = html.Div()
