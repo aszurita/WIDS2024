a99f99b0718921b31b8b0b5522ed2da81019683f
Luis R. <lromeror@fiec.espol.edu.ec>
2024-05-07 19:55:40 -0500

    Cambios en greaficos

--- pages/analisis.py
+++ pages/analisis.py
@@ -16,6 +16,7 @@ training_df  = pd.read_csv("assets/data/training.csv")
 corre_df=pd.read_csv("assets/data/correlaciones.csv")
 df_encoder = pd.read_csv('assets/data/df_encoder.csv')
 df_states=pd.read_csv('assets/data/df_states.csv')
+df_race=pd.read_csv('assets/data/df_Race.csv')
 
 links = html.Div([
     html.A(html.Label('Datos',className='link'),href='#datos'),
@@ -495,11 +496,33 @@ labels_parti=html.Div([
 def drop_part(value):
     return scatter_parti(df_states,value)
 
-titu_tiParti=html.H1("Analisis por Raza",className='title_Corre')
+def fig_race(df_race):
+    bar_race = px.bar(
+    df_race.sort_values('Percentage', ascending=False),
+    y=['Yes_Diag_PercentageTotal', 'No_Diag_PercentageTotal'], x='patient_race', title="Distribution of Diagnosis by Race",
+    labels={'patient_race': 'patient_race'.title(), 'value': 'Percentage', 'variable': 'Diagnosis Type','Yes_Diag_PercentageTotal':'Yes_DiagPeriodL90D',
+            'No_Diag_PercentageTotal':'No_DiagPeriodL90D'},
+    text_auto=True,
+    color_discrete_sequence=px.colors.qualitative.Pastel,
+    hover_data=['Total_Count'],
+    barmode='group',
+    )
+    for trace in bar_race.data:
+        if trace.name == 'Yes_Diag_PercentageTotal':
+            trace.name = 'YesDiagPeriodL90D'
+        elif trace.name == 'No_Diag_PercentageTotal':
+            trace.name = 'NoDiagPeriodL90D'
+    return bar_race
 
+titu_Race=html.H1("Analisis por Raza",className='title_Corre')
+reace_fig=html.Div([
+        html.Div([
+            dcc.Graph(figure=fig_race(df_race))
+        ],className='rounded-graph')    
+    ])
 layout = html.Div(
     [links,titulo,tipo_datos,fast_analisis,div_correlation,div_graficas_features,
     final,subTitulo,div_graficorre,analisi_corre,tiuloMap,grfMapa,sub_tiuloMap,top_state,
-    sub_tituBmi,state_bmi,titu_tiParti,state_parti,sub_tituParti,labels_parti]
+    sub_tituBmi,state_bmi,titu_tiParti,state_parti,sub_tituParti,labels_parti,titu_Race,reace_fig]
     ,className='center body'
     )
